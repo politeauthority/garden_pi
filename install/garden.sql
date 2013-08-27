080419ab5e7c9d4abd8fc4f47c8df5d4ceaf2499
Alix Fullerton <alix@politeauthority.com>
2013-08-27 15:34:59 -0600

    Added SQL statement to install for new ACL tables, started ACL Helper Class

--- install/garden.sql
+++ install/garden.sql
@@ -4,17 +4,6 @@
 -- ------------------------------------------------------
 -- Server version	5.5.31-0+wheezy1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `device_runtime`
 --
@@ -38,8 +27,7 @@ CREATE TABLE `device_runtime` (
 --
 
 DROP TABLE IF EXISTS `devices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `devices` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
@@ -55,8 +43,7 @@ CREATE TABLE `devices` (
 --
 
 DROP TABLE IF EXISTS `devices_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `devices_log` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `device_id` varchar(200) NOT NULL,
@@ -72,8 +59,7 @@ CREATE TABLE `devices_log` (
 --
 
 DROP TABLE IF EXISTS `garden_options`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `garden_options` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `meta_key` varchar(200) NOT NULL,
@@ -82,15 +68,14 @@ CREATE TABLE `garden_options` (
   `help_text` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
 -- Table structure for table `weather`
 --
 
 DROP TABLE IF EXISTS `weather`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `weather` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `indoor_temp_f` varchar(10) DEFAULT NULL,
@@ -101,15 +86,52 @@ CREATE TABLE `weather` (
   `date` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1546 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/* ACL SYSTEM */
+CREATE TABLE `acl_app` (
+  `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `restore` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
 
--- Dump completed on 2013-08-09 16:12:32
+ CREATE TABLE `acl_permissions` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `permKey` varchar(30) NOT NULL,
+  `permName` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `permKey` (`permKey`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `acl_role_perms` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `roleID` bigint(20) NOT NULL,
+  `permID` bigint(20) NOT NULL,
+  `value` tinyint(1) NOT NULL DEFAULT '0',
+  `addDate` datetime NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `roleID_2` (`roleID`,`permID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `acl_roles` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `roleName` (`roleName`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
+
+ CREATE TABLE `acl_user_perms` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` bigint(20) NOT NULL,
+  `permID` bigint(20) NOT NULL,
+  `value` tinyint(1) NOT NULL DEFAULT '0',
+  `addDate` datetime NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `userID` (`userID`,`permID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `acl_user_roles` (
+  `userID` bigint(20) NOT NULL,
+  `roleID` bigint(20) NOT NULL,
+  `addDate` datetime NOT NULL,
+  UNIQUE KEY `userID` (`userID`,`roleID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 

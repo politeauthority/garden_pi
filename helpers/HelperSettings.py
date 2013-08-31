ebb0bbc4dfe380d0aa3622d99259b2b8c46b3662
Alix Fullerton <alix@nakedsun.(none)>
2013-08-31 00:28:55 -0600

    Added create mysql tables to install.py, install inserts still not working however

--- helpers/HelperSettings.py
+++ helpers/HelperSettings.py
@@ -18,8 +18,27 @@ Mysql = MVC.loadDriver('Mysql')
 class HelperSettings( object ):
 
   def __init__( self ):
-    self.table_name = 'garden_options'
+    self.table_name = 'options'
 
+  def get_option( self, meta_key, bool = False ):
+    options = Mysql.ex( "SELECT * FROM %s.%s WHERE meta_key = '%s';" % ( MVC.db['name'], self.table_name, meta_key ) )
+    try:
+      options[0]
+    except Exception:
+      return False
+
+    if bool:
+      if options[0] == '1':
+        return True
+      elif options[0] == '0':
+        return False
+    else:
+      return options[0][2]
+
+  def get_options( self ):
+    options = Mysql.ex( "SELECT * FROM %s.%s ORDER BY `meta_key`;" % ( MVC.db['name'], self.table_name ) )
+    return options
+  
   def update( self, meta_key, meta_value ):
     value_check = Mysql.ex( "SELECT * FROM %s.%s WHERE meta_key = '%s' " %
      ( MVC.db['name'], self.table_name, meta_key ) );
@@ -29,29 +48,21 @@ class HelperSettings( object ):
       sql = "INSERT INTO %s.%s ( meta_key, meta_value ) VALUES ( '%s', '%s' );" % (  MVC.db['name'], self.table_name, meta_key, meta_value )
     return Mysql.ex( sql )
 
-  def get_options( self ):
-    options = Mysql.ex( "SELECT * FROM %s.%s ORDER BY `meta_key`;" % ( MVC.db['name'], self.table_name ) )
-    return options
-  
-  def get_option( self, meta_key, bool = False ):
-    options = Mysql.ex( "SELECT * FROM %s.%s WHERE meta_key = '%s';" % ( MVC.db['name'], self.table_name, meta_key ) )
-    try:
-      options[0]
-    except Exception:
-      print Exception
-
-    if bool:
-      if options[0] == '1':
-        return True
-      elif options[0] == '0':
-        return False
-    else:
-      return options[0][2]
-  
   # Bulk is dictionary
   def bulk_update( self, bulk ):
     for meta_key, meta_value in bulk.items():
       meta_key = meta_key.replace("settings['", '').replace("']", '')
       self.update( meta_key, meta_value )
+
+  def insert( self, meta_key, meta_value, pretty_name, help_text, parent = 0 ):
+    option = {
+      'meta_key'    : meta_key,
+      'meta_value'  : meta_value,
+      'pretty_name' : pretty_name,
+      'help_text'   : help_text,
+      'parent'      : parent
+    }
+    Mysql.insert( self.table_name, option )
+    return self.get_option( meta_key )
 
 # End File: helpers/HelperSettings.py
--- install/install.py
+++ install/install.py
@@ -11,19 +11,150 @@ MVC = MVC()
 import subprocess
 
 
-subprocess.call( "apt-get install arduino",    shell=True )
-subprocess.call( "apt-get install python-serial",   shell=True )
+# subprocess.call( "apt-get install arduino",    shell=True )
+# subprocess.call( "apt-get install python-serial",   shell=True )
 
-subprocess.call( "apt-get install python-cherrypy", shell=True )
-subprocess.call( "apt-get install python-jinja2",   shell=True )
-subprocess.call( "apt-get install python-dev",      shell=True )
+# subprocess.call( "apt-get install python-cherrypy", shell=True )
+# subprocess.call( "apt-get install python-jinja2",   shell=True )
+# subprocess.call( "apt-get install python-dev",      shell=True )
 
-subprocess.call( "apt-get install python-cherrypy", shell=True )
+# subprocess.call( "apt-get install python-cherrypy", shell=True )
 
-if MVC.raspberry_pi:
-    subprocess.call( "apt-get install python-rpi.gpio", shell=True )
+# if MVC.raspberry_pi:
+#     subprocess.call( "apt-get install python-rpi.gpio", shell=True )
 
 
 
-subprocess.call( "mysql -h%s -u%s -p%s garden < %s" % ( MVC.db['host'], MVC.db['user'], MVC.db['pass'], MVC.garden_dir + 'install/garden.sql' ), shell=True )
-subprocess.call( "mysql -h%s -u%s -p%s garden < %s" % ( MVC.db['host'], MVC.db['user'], MVC.db['pass'], MVC.garden_dir + 'install/garden_options.sql' ), shell=True )
+# Create the SQL tables we require
+
+Mysql = MVC.loadDriver('Mysql')
+
+
+# Base Website tables
+createTable_options = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`options` (
+  `id` 					int(9) NOT NULL AUTO_INCREMENT,
+  `meta_key` 		varchar(200) NOT NULL,
+  `meta_value` 	varchar(200) NOT NULL,
+  `parent`    	int(10) DEFAULT 0,
+  `pretty_name` varchar(250) DEFAULT NULL,
+  `help_text` 	varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+); """
+
+# createTable_devices = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`devices` (
+#   `id` 					id(9) NOT NULL AUTO_INCREMENT,
+#   `name` 				varchar(200) NOT NULL,
+#   `status_bit`	int(1) NOT NULL,
+#   `outlet_num` 	int(10) DEFAULT NULL,
+#   `type` 				varchar(250) DEFAULT NULL,
+#   PRIMARY KEY (`id`)
+# ); """
+
+
+# User tables
+createTable_users  = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`users` (
+  `id` 					int(9)       NOT NULL AUTO_INCREMENT,
+  `user` 				varchar(100) NOT NULL,
+  `email` 			varchar(250) NOT NULL,
+  `pass` 				varchar(250) NOT NULL,
+  `last_login` 	varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);"""
+
+createTable_usermeta = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`usermeta` (
+  `id` 					int(9) NOT NULL AUTO_INCREMENT,
+  `user_id` 		int(10) NOT NULL,
+  `parent` 			int(10) NOT NULL,
+  `meta_key` 		varchar(200) NOT NULL,
+  `meta_value` 	varchar(200) NOT NULL,
+  `pretty_name` varchar(250) DEFAULT NULL,
+  `help_text`		varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+); """
+
+createTable_user_acl_permissions = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`acl_permissions` (
+  `id` 				bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `permKey` 	varchar(30) NOT NULL,
+  `permName` 	varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `permKey` (`permKey`)
+); """
+
+createTable_user_acl_role_perms  = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`acl_role_perms` (
+  `id` 			bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `roleID`	bigint(20) NOT NULL,
+  `permID` 	bigint(20) NOT NULL,
+  `value` 	tinyint(1) NOT NULL DEFAULT '0',
+  `addDate` datetime NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `roleID_2` (`roleID`,`permID`)
+); """
+
+createTable_user_acl_roles       = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`acl_roles` (
+  `id` 				int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `roleName`	varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `roleName` (`roleName`)
+); """
+
+createTable_user_acl_user_perms  = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`acl_user_perms` (
+  `id` 			    bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userID`	    bigint(20) NOT NULL,
+  `permID` 	    bigint(20) NOT NULL,
+  `value` 	    tinyint(1) NOT NULL DEFAULT '0',
+  `addDate` datetime NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `userID` (`userID`,`permID`)
+ ); """
+
+createTable_user_acl_user_roles  = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`acl_user_roles` (
+  `userID`      bigint(20) NOT NULL,
+  `roleID`      bigint(20) NOT NULL,
+  `addDate`     datetime NOT NULL,
+  UNIQUE KEY `userID` (`userID`,`roleID`)
+); """
+
+
+# Sensor reading tables
+createTable_weather = """CREATE TABLE `"""+ MVC.db['name'] +"""`.`weather` (
+	`id` 										int(9) NOT NULL AUTO_INCREMENT,
+	`indoor_temp_f` 				varchar(10) DEFAULT NULL,
+  `indoor_humidity` 			varchar(50) DEFAULT NULL,
+  `outdoor_temp_f` 				varchar(50) DEFAULT NULL,
+  `outdoor_temp_f_feels`	varchar(50) DEFAULT NULL,
+  `outdoor_humidity` 			varchar(60) DEFAULT NULL,
+  `date` 									varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);"""
+
+# @todo: make a backup of the db if somethings there
+Mysql.ex( "DROP DATABASE IF EXISTS `%s`;" % MVC.db['name'] )
+Mysql.ex( "CREATE DATABASE IF NOT EXISTS `%s`;" % MVC.db['name'] )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.options' % MVC.db['name']  )
+Mysql.ex( createTable_options )
+# Mysql('DROP TABLE IF EXISTS %s.devices' % MVC.db['name']  )
+# Mysql.ex( createTable_devices )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.users' % MVC.db['name']  )
+Mysql.ex( createTable_users )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.usermeta' % MVC.db['name']  )
+Mysql.ex( createTable_usermeta )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.acl_permissions' % MVC.db['name']  )
+Mysql.ex( createTable_user_acl_permissions )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.acl_role_perms' % MVC.db['name']  )
+Mysql.ex( createTable_user_acl_role_perms )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.acl_roles' % MVC.db['name']  )
+Mysql.ex( createTable_user_acl_roles )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.acl_user_perms' % MVC.db['name']  )
+Mysql.ex( createTable_user_acl_user_perms )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.acl_user_roles' % MVC.db['name']  )
+Mysql.ex( createTable_user_acl_user_roles )
+Mysql.ex( 'DROP TABLE IF EXISTS %s.weather' % MVC.db['name']  )
+Mysql.ex( createTable_weather )
+
+
+Settings = MVC.loadHelper('Settings')
+
+print Settings.insert( 'site-url',         'http',  'Site Url',     'asdasd', 0 )
+print Settings.insert( 'use-sensor-shtx',  '0', 'SHT1x Sensor', 'Use the temperature humidity sensor'  )
+
+#End File: install/install.py

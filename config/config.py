66feda00e17f5e1104489960dfd57a994f28b6f0
Alix Fullerton <alix@politeauthority.com>
2013-09-05 11:51:39 -0600

    update to the config file

---
+++ config/config.py
@@ -0,0 +1,16 @@
+#Primary config file, before install this is the only file you must modify
+
+settings = { 
+    'database': {
+        'host'       : "localhost",
+        'user'       : "",
+        'pass'       : "",
+        'name'       : "garden",
+    },
+    'webserver':{
+        'host_ip'    : "",
+        'host_port'  : 8888,
+    },
+}
+
+# Endfile: config/config.py
